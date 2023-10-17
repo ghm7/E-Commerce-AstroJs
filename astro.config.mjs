e7e6215c4ebb5692430ee7baf47a2c2684469c14
ghm7 <pozzisgnosis@outlook.com>
2023-10-17 09:36:00 -0300

    Deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ghm7.github.io/',
+  base: '/E-Commerce-AstroJs',
+});
