ae10faff4a457ec9cd4dd56202d81afb3cbfc6d2
ghm7 <pozzisgnosis@outlook.com>
2023-10-17 09:46:35 -0300

    Add image files & login build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ghm7.github.io/',
-  base: '/E-Commerce-AstroJs',
+  // base: '/E-Commerce-AstroJs',
 });
