42766768389e405c6f7d4e21aa9310f6509421fd
ghm7 <pozzisgnosis@outlook.com>
2023-11-04 05:02:13 -0300

    Design product-info components

--- src/js/services/getData.js
+++ src/js/services/getData.js
@@ -1,9 +1,45 @@
+// const getData = async ({ URL, endpoint }) => {
+//   if (endpoint !== undefined) {
+//     return fetch(`${URL}/${endpoint}.json`)
+//       .then((response) => response.json())
+//       .then((data) => data);
+//   } else {
+//     return fetch(`${URL}.json`)
+//       .then((response) => response.json())
+//       .then((data) => data);
+//   }
+// };
+
+import { PRODUCTS_URL, CATEGORIES_URL } from '../constants/API.js';
+
 const getData = async ({ URL, endpoint }) => {
+  if (endpoint === 'ALL') {
+    const categories = await fetch(`${CATEGORIES_URL}.json`)
+      .then((response) => response.json())
+      .then((data) => data);
+
+    const allProducts = await Promise.all(
+      categories.map(async ({ id }) => {
+        const { products } = await fetch(
+          `${PRODUCTS_URL}/${id.toString()}.json`
+        )
+          .then((response) => response.json())
+          .then((data) => data);
+
+        return products;
+      })
+    );
+
+    return allProducts.filter((product) => product.length !== 0).flat(Infinity);
+  }
+
   if (endpoint !== undefined) {
     return fetch(`${URL}/${endpoint}.json`)
       .then((response) => response.json())
       .then((data) => data);
-  } else {
+  }
+
+  if (endpoint === undefined) {
     return fetch(`${URL}.json`)
       .then((response) => response.json())
       .then((data) => data);
